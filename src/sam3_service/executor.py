Fixes for float32 tiff normalization

src/sam3_service/executor.py
@@ -1,4 +1,3 @@
-import numpy as np
 import logging
 import multiprocessing as mp
 from concurrent.futures import Future, ThreadPoolExecutor
@@ -169,11 +168,7 @@ class SAM3Worker(MP_CONTEXT.Process):  # type: ignore[unsupported-base]
 
     def handle_image_request(self, task_id: TaskId, task: ProcessImageRequest) -> None:
         with smart_open.open(task.image_uri, "rb") as fp:
-            # NB: a typical tomographic reconstruction is a monochromatic TIFF of dtype float32
-            # with values in teh range [-5.0e-5, 2.0e-4].  Calling PIL.Image.convert('RGB') will 
-            # indiscriminately squash this to a uint8 of pure zeroes.  You really want to do a 
-            # quantile normalization prior to feeding it to the SAM3 preprocessor!
-            image = np.array(Image.open(fp))
+            image = Image.open(fp)
             res = self.model.infer_image(image, task.text_prompt)
 
         resp = ProcessImageResponse(

src/sam3_service/io.py
@@ -17,10 +17,7 @@ logger = logging.getLogger(__name__)
 
 
 def tiff_decoder(data):
-    arr = tifffile.imread(io.BytesIO(data))
-    if arr.ndim == 2:
-        arr = arr[np.newaxis, :, :]
-    return arr
+    return tifffile.imread(io.BytesIO(data))
 
 
 def _expand_prompts(samples: Iterable[dict[str, Any]]) -> Iterator[SamplePrompt]:

src/sam3_service/model.py
@@ -2,6 +2,8 @@ import logging
 from time import perf_counter
 from typing import Any
 
+import numpy as np
+import numpy.typing as npt
 import torch
 from PIL import Image
 from torch.utils.data import DataLoader, IterableDataset
@@ -12,15 +14,35 @@ from .schema import SAM3Result, SamplePrompt
 
 logger = logging.getLogger(__name__)
 processor = Sam3Processor.from_pretrained(CHECKPOINT_DIR, local_files_only=True)
+NDArray = npt.NDArray[Any]
 
-def quantile_norm(img):
-    import numpy as np
-    p_lo, p_hi = np.percentile(img, [1, 99])
-    img = np.clip((img - p_lo) / (p_hi - p_lo), 0, 1)
 
-    # Convert to uint8 0-255 range, which is what vision preprocessors expect
-    img = (img * 255).astype(np.uint8)
-    return img[np.newaxis,:,:]
+def quantile_norm(image: NDArray | Image.Image) -> NDArray:
+    if isinstance(image, Image.Image):
+        if image.mode == "RGBA":
+            image = image.convert("RGB")
+        image = np.array(image)
+
+    # Vision models expect a color channel:
+    if image.ndim == 2:
+        image = image[np.newaxis, :, :]
+
+    assert image.ndim == 3
+
+    if image.shape[2] == 4:
+        image = image[:, :, :3]
+    elif image.shape[0] == 4:
+        image = image[:3, :, :]
+
+    # If floating dtype, normalize and clamp pixel intensities such that
+    # the 1st percentile is 0 and 99th percentile is 255
+    if not np.issubdtype(image.dtype, np.integer):
+        p_lo, p_hi = np.percentile(image, [1, 99])
+        image = np.clip((image - p_lo) / (p_hi - p_lo), 0, 1)
+        image = (image * 255).astype(np.uint8)
+
+    return image
+
 
 def preprocess(samples: list[SamplePrompt]) -> tuple[list[SamplePrompt], BatchEncoding]:
     """
@@ -70,10 +92,9 @@ class Sam3Wrapper:
     @torch.autocast("cuda", dtype=torch.bfloat16)
     @torch.inference_mode()
     def infer_image(self, image: Image.Image, text_prompt: str) -> SAM3Result:
-        image = quantile_norm(image)
-        inputs = processor(images=image, text=text_prompt, return_tensors="pt").to(
-            self.device
-        )
+        inputs = processor(
+            images=quantile_norm(image), text=text_prompt, return_tensors="pt"
+        ).to(self.device)
         outputs = self.model(**inputs)
         r = processor.post_process_instance_segmentation(
             outputs,
@@ -93,8 +114,8 @@ class Sam3Wrapper:
     def infer_batch(
         self,
         dataset: IterableDataset[SamplePrompt],
-        threshold: float = 0.3,
-        mask_threshold: float = 0.3,
+        threshold: float = 0.5,
+        mask_threshold: float = 0.5,
         batch_size: int = 16,
         loader_num_workers: int = 2,
     ) -> list[SAM3Result]: