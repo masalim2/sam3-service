b29728fc2b536e8adb0d3e06a600262878e0c921
Misha Salim <msalim@anl.gov>
2026-03-24 20:50:36 +0000

    Add Globus Compute func registration script

---
+++ scripts/register_globus_compute_func.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+
+from globus_compute_sdk import Client
+
+gcc = Client()
+
+# sophia-sam3 endpoint @ /home/openinference_svc/
+ENDPOINT_ID = "25662117-d605-4f43-af9b-1b9ae390e4e2"
+BASE_URL = "http://localhost:8000"
+
+
+def submit_batch(dataset_path: str | Path):
+    from pathlib import Path
+
+    import requests
+
+    dataset_path = Path(dataset_path).resolve()
+    if not dataset_path.is_file():
+        raise FileNotFoundError(f"Could not locate {dataset_path}")
+    if dataset_path.suffix != ".tar":
+        raise ValueError(f"Expected .tar file extension: {dataset_path}")
+
+    resp = requests.post(
+        f"{BASE_URL}/process-batch",
+        json={"dataset_path": dataset_path.as_posix()},
+    )
+    resp.raise_for_status()
+    return resp.json()
+
+
+def submit_image(image_uri: str, prompt: str):
+    from pathlib import Path
+
+    import requests
+
+    if "://" not in image_uri:
+        image_uri = "file://" + Path(image_uri).resolve().as_posix()
+
+    resp = requests.post(
+        f"{BASE_URL}/process-image",
+        json={"image_uri": image_uri, "prompt": {"text": prompt}},
+    )
+    resp.raise_for_status()
+    return resp.json()
+
+
+for func in [submit_batch, submit_image]:
+    func_uuid = gcc.register_function(func)
+    print(f"Registered {func.__name__}: {func_uuid=}")
