9389fdcd6f11ddf8995f673a2ae9ff8fa55e6bfb
Misha Salim <msalim@anl.gov>
2026-03-25 18:34:12 +0000

    update test

--- scripts/test_submit_funcs.py
+++ scripts/test_submit_funcs.py
@@ -4,7 +4,7 @@ gcc = Client()
 
 # sophia-sam3 endpoint @ /home/openinference_svc/
 ENDPOINT_ID = "25662117-d605-4f43-af9b-1b9ae390e4e2"
-FUNC_ID = "26ff10a2-c944-4f1d-bf53-d3df96bf5baa"
+FUNC_ID = "7c6ed5c4-8a7a-4cd6-834b-b653680c1475"
 
 with Executor(
     endpoint_id=ENDPOINT_ID,
@@ -15,10 +15,10 @@ with Executor(
 ) as gce:
     fut = gce.submit_to_registered_function(
         function_id=FUNC_ID,
-        kwargs={
+        args=[{
             "inference_type": "batch",
-            "dataset_path": "/eagle/inference_service/sam3-service/examples/wds-test/shard-00000.tar",
-        },
+            "data_uri": "/eagle/inference_service/sam3-service/examples/wds-test/shard-00000.tar",
+        }],
     )
     print(f"Submitted batch inference {fut.task_id=}")
     print("Awaiting results...")
@@ -27,10 +27,11 @@ with Executor(
 with Executor(endpoint_id=ENDPOINT_ID, client=gcc) as gce:
     fut = gce.submit_to_registered_function(
         function_id=FUNC_ID,
-        kwargs=dict(
-            image_uri="https://raw.githubusercontent.com/masalim2/sam3-service/refs/heads/main/examples/images/groceries.jpg",
-            prompt="grocery bag",
-        ),
+        args=[{
+            "inference_type": "single-image",
+            "data_uri": "https://raw.githubusercontent.com/masalim2/sam3-service/refs/heads/main/examples/images/groceries.jpg",
+            "single_image_prompt": "grocery bag",
+        }],
     )
     print(f"Submitted single image inference {fut.task_id=}")
     print(fut.result(timeout=300))
